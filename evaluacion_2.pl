1bc48bacc1bff3224e356c93d9533a9d2ab9e879
CharFranR <oscar.reyes@est.ulsa.edu.ni>
2026-01-30 13:10:33 -0600

    evaluacion 2

--- evaluacion_2.pl
+++ evaluacion_2.pl
@@ -1,21 +1,43 @@
 % Hechos
 
-progenitor(maria,oscar).
 progenitor(esther,maria).
 progenitor(carlos,maria).
 
-progenitor(oscar_padre,oscar).
+progenitor(esther,william).
+progenitor(carlos,william).
+progenitor(esther,ervin).
+progenitor(carlos,ervin).
+
 progenitor(oscar_abuelo,oscar_padre).
 progenitor(marixa,oscar_padre).
 
+progenitor(william,leonie).
+progenitor(william,alexander).
+progenitor(william,cedrick).
+
+progenitor(oscar_padre,oscar).
+progenitor(maria,oscar).
+
+progenitor(ervin,keneri).
+progenitor(ervin,leonela).
+
+% Género
 hombre(oscar).
 hombre(oscar_padre).
 hombre(oscar_abuelo).
 hombre(carlos).
+hombre(william).
+hombre(ervin).
+hombre(alexander).
+hombre(cedrick).
+hombre(keneri).
 
 mujer(maria).
 mujer(esther).
 mujer(marixa).
+mujer(leonie).
+mujer(leonela).
+
 
 % Reglas
 
@@ -33,6 +55,12 @@ es_padre(X):- progenitor(X,_), hombre(X).
 es_abuelo(X):- progenitor(X,Z), progenitor(Z,_),hombre(X).
 es_abuela(X):- progenitor(X,Z), progenitor(Z,_),mujer(X).
 
+es_primo(X):- progenitor(Z,X), progenitor(W,_), progenitor(P,Z), progenitor(P,W), Z \= W, hombre(X).
+es_prima(X):- progenitor(Z,X), progenitor(W,_), progenitor(P,Z), progenitor(P,W), Z \= W, mujer(X).
+
+es_tio(X):- progenitor(P,X), progenitor(P,Z), progenitor(Z,_), X \= Z, hombre(X).
+es_tia(X):- progenitor(P,X), progenitor(P,Z), progenitor(Z,_), X \= Z, mujer(X).
+
 % Reglas (X,Y)
 
 es_hijo_de(X,Y):- progenitor(Y,X), hombre(X).
@@ -46,3 +74,9 @@ es_abuela_de(X,Y):- progenitor(X,Z), progenitor(Z,Y),mujer(X).
 
 es_hermano_de(X,Y):- progenitor(Z,X), progenitor(Z,Y), hombre(X), X \= Y.
 es_hermana_de(X,Y):- progenitor(Z,X), progenitor(Z,Y), mujer(X), X \= Y.
+
+es_primo_de(X,Y):- progenitor(Z,X), progenitor(W,Y), progenitor(P,Z), progenitor(P,W), Z \= W, hombre(X).
+es_prima_de(X,Y):- progenitor(Z,X), progenitor(W,Y), progenitor(P,Z), progenitor(P,W), Z \= W, mujer(X).
+
+es_tio_de(X,Y):- progenitor(P,X), progenitor(P,Z), progenitor(Z,Y), X \= Z, hombre(X).
+es_tia_de(X,Y):- progenitor(P,X), progenitor(P,Z), progenitor(Z,Y), X \= Z, mujer(X).
