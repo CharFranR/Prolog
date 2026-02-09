da456cecbe47cff2e91689e59f207c0d5e067d65
CharFranR <oscar.reyes@est.ulsa.edu.ni>
2026-02-09 11:15:30 -0600

    ejercicios en clase 2

--- BaseDeConocimiento.pl
+++ BaseDeConocimiento.pl
@@ -5,4 +5,4 @@ minimo(X,Y,Y):- Y=<X.
 
 % Ejercicio 2.
 
-notas:- write('Ingrese la nota: '), read(P), (P =< 69 -> write('Aprobado') ; P >= 70, P =< 79 -> write('Bueno') ; P >= 80, P =< 89 -> write('Muy bueno') ; P >= 90, P =< 100 -> write('Excelente') ; write('Nota inválida')).
+notas:- write('Ingrese la nota: '), read(P), (P =< 69 -> write('Regular') ; P >= 70, P =< 79 -> write('Bueno') ; P >= 80, P =< 89 -> write('Muy bueno') ; P >= 90, P =< 100 -> write('Excelente') ; write('Nota inválida')).
