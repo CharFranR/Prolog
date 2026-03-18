507388d0f7b88c14eee612ecf92704bf2520c0af
Oscar Francisco Reyes Guevara <oscar.reyes@est.ulsa.edu.ni>
2026-03-18 10:44:19 -0600

    ejercicio tarea ficheros

--- familia/familia.pl
+++ familia/familia.pl
@@ -1,3 +1,8 @@
 copiar :- get0(X), ((X =\= -1, put(X),copiar);X == -1).
 
 familia :- tell('nuevo_archivo.txt'), see('familia.txt'), copiar, seen, told,  tell(user), see('nuevo_archivo.txt'), copiar, seen.
+
+agregar_familiar(Nombre) :- see('familia.txt'), tell('temp.txt'), copiar, nl, write(Nombre), write('.'), told, seen, see('temp.txt'), tell('familia.txt'), copiar, told, seen.
+
+
+mostrar_archivo :- see('familia.txt'), copiar, seen.              
