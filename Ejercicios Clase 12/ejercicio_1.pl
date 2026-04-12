d487cda492b65bcf6091346d017c5bfb3c873186
Oscar Francisco Reyes Guevara <oscar.reyes@est.ulsa.edu.ni>
2026-04-11 18:00:19 -0600

    fix something

--- Ejercicios Clase 12/ejercicio_1.pl
+++ Ejercicios Clase 12/ejercicio_1.pl
@@ -1,28 +1,53 @@
 :- use_module(library(pce)).
 
+/*
+ * Ejercicio 1: Ventana de dialogo para ingresar datos personales
+ * Campos: nombres, apellidos, edad, carrera, anio de egreso
+ * Muestra los datos en la Consola de Prolog
+ */
+
 datos_personales :-
     new(Ventana, dialog('Datos Personales')),
     
+    % Crear campos de texto para los datos
     new(Nombres, text_item('Nombres')),
     new(Apellidos, text_item('Apellidos')),
     new(Edad, int_item('Edad')),
     new(Carrera, text_item('Carrera')),
     new(AnioEgreso, int_item('Anio de Egreso')),
     
+    % Anadir elementos a la ventana
     send(Ventana, append(Nombres)),
     send(Ventana, append(Apellidos)),
     send(Ventana, append(Edad)),
     send(Ventana, append(Carrera)),
     send(Ventana, append(AnioEgreso)),
+    
+    % Boton Mostrar Datos - pasar las referencias de los items
     send(Ventana, append(button('Mostrar Datos',
-        message(@prolog, mostrar_datos)))),
+        message(@prolog, mostrar_datos, Nombres, Apellidos, Edad, Carrera, AnioEgreso)))),
+    
+    % Boton Salir
     send(Ventana, append(button('Salir',
         message(Ventana, destroy)))),
     
+    % Abrir la ventana
     send(Ventana, open).
 
-mostrar_datos :-
+% Capturar los valores de cada item y mostrarlos en consola
+mostrar_datos(N, A, E, C, Anio) :-
+    get(N, selection, Nombres),
+    get(A, selection, Apellidos),
+    get(E, selection, Edad),
+    get(C, selection, Carrera),
+    get(Anio, selection, AnioEgreso),
     nl,
-    writeln('=== PRESIONASTE MOSTRAR DATOS ==='),
-    writeln('Por favor llena los campos y presiona el boton.'),
+    writeln('=== DATOS PERSONALES ==='),
+    writeln('------------------------'),
+    write('Nombres: '), writeln(Nombres),
+    write('Apellidos: '), writeln(Apellidos),
+    write('Edad: '), writeln(Edad),
+    write('Carrera: '), writeln(Carrera),
+    write('Anio de Egreso: '), writeln(AnioEgreso),
+    writeln('------------------------'),
     nl.
